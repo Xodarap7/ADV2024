004abc63589377153c7b91d899cfb3c1544f715e
Ярослав <xodarap7@gmail.com>
2024-12-01 14:20:25 +0500

    первый день, 2 звезды из 2х

---
+++ 1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	filename := "day1.txt"
+
+	totalDistance, similarity, err := Start(filename)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+
+	fmt.Println("Сумма расстояний между парами чисел:", totalDistance)
+	fmt.Println("Оценка сходства:", similarity)
+}
